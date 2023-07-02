374c4c66294ae6046402449468fd6c20362ce4a9
cezicoder <cezarylachut1981@gmail.com>
2023-07-02 21:28:54 +0200

    add new project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -31,21 +31,22 @@ export const Projects = () => {
         <motion.div initial='out' animate='in' exit='out' variants={animationTwo} transition={{ duration: 2 }}>
             <div className='flex items-center justify-center'>
 
-                <div className='overflow-y-auto p-4 mt-40 w-[80%] h-60 shadow-2xl flex items-center justify-center '>
-                    <p className='italic bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-pink-600'>
-                        <h4 className='m-4'>Project:</h4>
-                        
+                <div className=' p-8 mt-40 w-[80%] h-60 shadow-2xl flex flex-col items-center justify-center '>
+                    <h4 className='m-4'>Project:</h4>
+                    <p className='italic bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-pink-600 m-2'>
+
+
                         <a target='_blank' href='https://cezi-chat-gpt.vercel.app'>Cezi-ChatGPT</a>
                         <hr></hr>
                     </p>
-                    <p className='break-words'></p>
-                    <p className='italic bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-pink-600'>
-                        <h4 className='m-4'>Project:</h4>
 
-                        <a target='_blank' href='image-uploader-client-blush.vercel.app'>Image-uploader</a>
+                    <p className='italic bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-pink-600 m-2'>
+
+
+                        <a target='_blank' href='https://image-uploader-client-blush.vercel.app'>Image-uploader</a>
                         <hr></hr>
                     </p>
-                    <p className='break-words'></p>
+
                 </div>
 
 
