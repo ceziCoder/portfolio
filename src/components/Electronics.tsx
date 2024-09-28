a83a5d4a4b64e8ebd9fe6da6de9441b5abfe922b
cezicoder <cezarylachut1981@gmail.com>
2024-09-28 19:12:53 +0200

    add nav to new page

--- src/components/Electronics.tsx
+++ src/components/Electronics.tsx
@@ -1,4 +1,5 @@
 import { Chrono } from "react-chrono";
+import { Navbar } from "../components/Navbar";
 
 
 
@@ -15,5 +16,26 @@ import { Chrono } from "react-chrono";
     },
   ];
 
-  
+
+
+export const Electronics = () => {
+  return (
+    
+    <Chrono
+          disableToolbar={true} // This hides the toolbar
+          disableNavOnKey={false} // Keep navigation using keyboard active
+          disableNavOnClick={false}
+          items={items}
+          mode="VERTICAL_ALTERNATING"
+          theme={{
+            primary: "rgba(2, 0, 0, 0.8)", // Black with transparency for the primary timeline bar
+            secondary: "rgba(0, 0, 0, 0.2)", // Darker transparent color for secondary elements
+            cardBgColor: "rgba(230, 60, 180, 0.3)", // Semi-transparent black background for cards
+            cardForeColor: "#000", // White text color inside the cards for contrast
+            titleColor: "#fff", // Title color for the cards
+            titleColorActive: "#2196f3", // Title color when active
+          }}
+    />
+  );
+};
 
--- src/page/Electronic.tsx
+++ src/page/Electronic.tsx
@@ -1,24 +1,13 @@
 import { Navbar } from "../components/Navbar";
+import { Electronics } from "../components/Electronics";
 
-import { Chrono } from "react-chrono";
-import { items } from "../components/Electronics"; // Correct path to the file where items are defined
 
 export const Electronic = () => {
-  return (
-    <Chrono
-          disableToolbar={true} // This hides the toolbar
-          disableNavOnKey={false} // Keep navigation using keyboard active
-          disableNavOnClick={false}
-          items={items}
-          mode="VERTICAL_ALTERNATING"
-          theme={{
-            primary: "rgba(2, 0, 0, 0.8)", // Black with transparency for the primary timeline bar
-            secondary: "rgba(0, 0, 0, 0.2)", // Darker transparent color for secondary elements
-            cardBgColor: "rgba(230, 60, 180, 0.3)", // Semi-transparent black background for cards
-            cardForeColor: "#000", // White text color inside the cards for contrast
-            titleColor: "#fff", // Title color for the cards
-            titleColorActive: "#2196f3", // Title color when active
-          }}
-    />
-  );
-};
+    return (
+        <div>
+        <Navbar />
+        <Electronics />
+          
+        </div>
+    );
+}
