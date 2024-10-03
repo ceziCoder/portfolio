3b54a2a39bf701e98019a18e9aae277bb5e375c9
cezicoder <cezarylachut1981@gmail.com>
2024-10-03 12:30:43 +0200

    new size

--- src/page/Keys.tsx
+++ src/page/Keys.tsx
@@ -12,6 +12,11 @@ import {
 } from "../components/Modal";
 import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
+import image1 from '../public/4-.jpg';
+import image2 from '../public/2-.jpg';
+import image3 from '../public/3-.jpg';
+import image4 from '../public/5-.jpg';
+import image5 from '../public/7-.jpg'
 
 export function Keys() {
   const testimonials = [
@@ -47,11 +52,26 @@ export function Keys() {
   ];
 
   const images = [
-    "https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1573790387438-4da905039392?q=80&w=3425&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1555400038-63f5ba517a47?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1554931670-4ebfabf6e7a9?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1546484475-7f7bd55792da?q=80&w=2581&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    {
+      src: image1,
+      description: "Launch",
+    },
+    {
+      src: image2,
+      description: "Launch",
+    },
+    {
+      src: image3,
+      description: "Autel  Xhorse",
+    },
+    {
+      src: image4,
+      description: "Xhorse",
+    },
+    {
+      src: image5,
+      description: "Launch",
+    },
   ];
 
   return (
@@ -64,18 +84,12 @@ export function Keys() {
               Moje narzedzia
             </span>
             <div className="-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
-              <FaTools size={30}/>
+              <FaTools size={30} />
             </div>
           </ModalTrigger>
           <ModalBody>
             <ModalContent>
-              <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-8">
 
-                <span className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 border border-gray-200">
-
-                </span>{" "}
-
-              </h4>
               <div className="flex justify-center items-center">
                 {images.map((image, idx) => (
                   <motion.div
@@ -84,59 +98,31 @@ export function Keys() {
                       rotate: Math.random() * 20 + 10,
                     }}
                     whileHover={{
-                      scale: 1.1,
+                      scale: 1.5,
                       rotate: 0,
                       zIndex: 100,
                     }}
                     whileTap={{
-                      scale: 1.1,
+                      scale: 1.5,
                       rotate: 0,
                       zIndex: 100,
                     }}
                     className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 flex-shrink-0 overflow-hidden"
                   >
                     <img
-                      src={image}
-                      alt="bali images"
-                      width="500"
-                      height="500"
+                      src={image.src}
+                      alt=""
+                      width="700"
+                      height="700"
                       className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
                     />
+                    <h6 className="text-neutral-700 dark:text-neutral-300 text-center mt-2 text-sm">
+                      {image.description}
+                    </h6>
                   </motion.div>
                 ))}
               </div>
-              <div className="py-10 flex flex-wrap gap-x-4 gap-y-6 items-start justify-start max-w-sm mx-auto">
-                <div className="flex  items-center justify-center">
-                  <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                    5 connecting flights
-                  </span>
-                </div>
-                <div className="flex items-center justify-center">
-                  <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                    12 hotels
-                  </span>
-                </div>
-                <div className="flex items-center justify-center">
-                  <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                    69 visiting spots
-                  </span>
-                </div>
-                <div className="flex  items-center justify-center">
-                  <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                    Good food everyday
-                  </span>
-                </div>
-                <div className="flex items-center justify-center">
-                  <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                    Open Mic
-                  </span>
-                </div>
-                <div className="flex items-center justify-center">
-                  <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                    Paragliding
-                  </span>
-                </div>
-              </div>
+
             </ModalContent>
           </ModalBody>
         </Modal>
